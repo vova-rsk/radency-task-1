755c6ebd48986c01131f4338ccbf04fc849052ec
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-02 13:56:35 +0300

    added base project structure

---
+++ src/js/components/constants.js
@@ -0,0 +1,25 @@
+const CATEGORIES = [
+    {
+        id: '1',
+        name: 'Task'
+    },
+    
+    {
+        id:'2',
+        name: 'Random Thought'
+    },
+    {
+        id: '3',    
+        name: 'Idea'
+    },
+        {
+        id: '4',    
+        name: 'Quote'
+    }
+];
+
+export const STATUS = {
+    ACTIVE: 'active',
+    ARCHIVED: 'archived',
+    DELETED: 'deleted'
+};
