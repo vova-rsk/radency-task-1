b0871c8f39f48f847c7dc2aaf945dfb83b6bfe99
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-03 22:03:26 +0300

    added correct icons visibility

--- src/js/components/notes.js
+++ src/js/components/notes.js
@@ -1,10 +1,46 @@
 import noteTmp from '../../templates/notes.hbs';
 import refs from './refs';
-import { STATUS } from './constants';
+import { CATEGORIES, STATUS, SVG_ICONS } from './constants';
 
 export function createActiveNotesTable(notes) {
     const filteredNotes = notes.filter(elem => elem.status === STATUS.ACTIVE);
     const notesList = noteTmp(filteredNotes);
     
     refs.activeNotesTable.insertAdjacentHTML('beforeend', notesList);
+}
+
+export function addCategoryIcons () { 
+    const categoryIcons = refs.getCategoryIcons();
+    
+    categoryIcons.forEach(icon => {
+        const category = icon.dataset.category;
+        const categoryObj = CATEGORIES.find(({ name }) => name === category);
+
+       icon.innerHTML = categoryObj.icon;
+    });
+}
+
+export function addCtrButtonsIcons() { 
+    const buttons = refs.getNoteCtrlButtons();
+
+    buttons.forEach(button => { 
+        const buttonName = button.getAttribute('name');
+        let iconToAdd = null;
+
+        switch (buttonName) { 
+            case 'edit':
+                iconToAdd = SVG_ICONS.EDIT;
+                break;
+            case 'to-archive':
+                iconToAdd = SVG_ICONS.BOX_ADD;
+                break;
+            case 'remove':
+                iconToAdd = SVG_ICONS.DELETE;
+                break;
+            default:
+                iconToAdd = SVG_ICONS.DEFAULT;
+        }
+        
+        button.innerHTML = iconToAdd;
+    });
 }
--- src/js/components/refs.js
+++ src/js/components/refs.js
@@ -1,5 +1,7 @@
 const refs = {
     activeNotesTable: document.querySelector('#table-notes-body'),
+    getCategoryIcons: () => document.querySelectorAll('.table__icon'),
+    getNoteCtrlButtons: () => document.querySelectorAll('#note-ctrl-btns button')
 };
 
 export default refs
