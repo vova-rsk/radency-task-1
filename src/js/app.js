26c6838a7baf396e7a3682afbeac5f0b593e3172
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-04-03 22:05:41 +0300

    replaced notes-storage

--- src/js/app.js
+++ src/js/app.js
@@ -1,77 +1,8 @@
-import { CATEGORIES, STATUS } from './components/constants';
-import { createActiveNotesTable } from './components/notes'
+import { createActiveNotesTable, addCategoryIcons, addCtrButtonsIcons } from './components/notes';
+import store from '../db';
 
-const notes = [
-    {
-        id:"1",
-        name: "Shopping list",
-        created: "April 20, 2021",
-        category: "Task",
-        categoryIconUrl:"./images/icons.svg#local_grocery_store",
-        content: "Tomatoes, bread",
-        dates: null,
-        status:"active",
-    },
-    {
-        id:"2",
-        name: "The theory of evolution",
-        created: "April 27, 2021",
-        category: "Random Thought",
-        categoryIconUrl:"./images/icons.svg#psychology",
-        content: "The evolution of the civilazation",
-        dates: null,
-        status:"active",
-    },
-    {   
-        id:"3",
-        name: "New Feature",
-        created: "May 05, 2021",
-        category: "Idea",
-        categoryIconUrl:"./images/icons.svg#lightbulb_outline",
-        content: "Implement new amaizing feature",
-        dates: ['03/05/2021','05/05/2021'],
-        status:"active",
-    },
-    {
-        id:"4",
-        name: "William Gaddis",
-        created: "May 07, 2021",
-        category: "Quote",
-        categoryIconUrl:"./images/icons.svg#format_quote",
-        content: "Power  doesn't corrupt people, people corrupt power",
-        dates: null,
-        status:"active",
-    },
-    {
-        id:"5",
-        name: "Books",
-        created: "May 15, 2021",
-        category: "Task",
-        categoryIconUrl:"local_grocery_store",
-        content: "The Lean Startup",
-        dates: null,
-        status:"active",
-    },
-    {   
-        id:"6",
-        name: "Practice",
-        created: "February 11, 2021",
-        category: "Idea",
-        categoryIconUrl:"./images/icons.svg#lightbulb_outline",
-        content: "get practice with Node.js",
-        dates: ["3/15/2022", "4/10/2022"],
-        status:"active",
-    },
-    {   
-        id:"7",
-        name: "Holidays",
-        created: "March 3, 2022",
-        category: "Random Thought",
-        categoryIconUrl:"./images/icons.svg#psychology",
-        content: "To meet friends",
-        dates: null,
-        status:"active",
-    }    
-];
-
-document.addEventListener('DOMContentLoaded', () => createActiveNotesTable(notes));
+document.addEventListener('DOMContentLoaded', () => {
+    createActiveNotesTable(store.notes);
+    addCategoryIcons();
+    addCtrButtonsIcons();
+});
